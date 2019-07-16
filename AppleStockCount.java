4d02844197cb8d134b82f87449ad4b5695ae2f11
ayolicias <ziyandandlamafa47@gmail.com>
2019-07-16 16:29:27 +0200

    2 decimal points added

--- AppleStockCount.java
+++ AppleStockCount.java
@@ -5,32 +5,29 @@ public static void main(String[] args){
         System.out.println("please give me qty!");
         return;
     }
-    int qty = Integer.parseInt(args[0]);
-    double cost = Double.parseDouble(args[1]);
-    double total = qty * cost;
-
-    if(qty < 1){
+        int qty = Integer.parseInt(args[0]);
+        double cost = Double.parseDouble(args[1]);
+        double total = qty * cost;
+        if(qty < 1){
         System.out.println(" Out of Stock");
     }
     else{
         StringBuilder message = new StringBuilder();
         message.append("You have bought ");
-        //check if it is a multiple of 12 and returns the amount divided by 12
+        //check if it is a multiple of dozens and returns the amount
         if(qty % 12 == 0){
             int dozen = qty / 12;
             message.append(dozen + " dozen");
         }
-        //check if it is a multiple of 36 and returns the amount divided by 36
-
-      message.append(" of apple");
-      if (qty > 1) {
+        message.append(" of apple");
+        if (qty > 1) {
         message.append("s");
       }
-      message.append(" for a total cost of ");
-      message.append(total);
-      message.append(".");
+        message.append(" for a total cost of ");
+        message.append(total);
+        message.append(".");
 
-      System.out.println(message.toString());
+        System.out.println(message.toString());
 
     }
 
