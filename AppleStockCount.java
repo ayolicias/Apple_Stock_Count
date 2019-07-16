b4844047f1f69ac12bb2db827518d8f24e8c6d3a
ayolicias <ziyandandlamafa47@gmail.com>
2019-07-16 16:18:36 +0200

    multiples of dozens of Apples Added

--- AppleStockCount.java
+++ AppleStockCount.java
@@ -6,14 +6,13 @@ public static void main(String[] args){
         return;
     }
     int qty = Integer.parseInt(args[0]);
-    double cost = 4.50;
-    double total;
+    double cost = Double.parseDouble(args[1]);
+    double total = qty * cost;
 
     if(qty < 1){
         System.out.println(" Out of Stock");
     }
     else{
-        total = qty * cost;
         StringBuilder message = new StringBuilder();
         message.append("You have bought ");
         //check if it is a multiple of 12 and returns the amount divided by 12
@@ -23,11 +22,6 @@ public static void main(String[] args){
         }
         //check if it is a multiple of 36 and returns the amount divided by 36
 
-         if(qty % 36 == 0){
-            int dozen = qty / 36;
-            message.append(dozen + " dozen");
-        }
-            //   message.append(qty);
       message.append(" of apple");
       if (qty > 1) {
         message.append("s");
